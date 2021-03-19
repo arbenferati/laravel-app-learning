e70a3e81b51b7618d283dbbe919ea44ba4ec2204
arben ferati <arben.ferati@cpnv.ch>
2021-03-19 01:20:57 +0100

    Last touch for navigation link on sidebar

--- resources/views/components/sidebar-nav-link.blade.php
+++ resources/views/components/sidebar-nav-link.blade.php
@@ -1,17 +1,18 @@
 @props(['first', 'last', 'active', 'disconnect'])
 
 @php
-$tmp = '';
-$classes = ($active ?? false)
-            ? 'text-center bg-gray-700 border-r-4 border-blue-300 py-6 hover:bg-gray-700 min-w-full'
-            : 'text-center bg-gray-800 py-6 hover:bg-gray-700 hover:border-blue-300 min-w-full';
 
-($first ?? false)
-            ? $classes .= ' rounded-t-lg'
-            : $classes .= '';
-($last ?? false)
-            ? $classes .= ' rounded-b-lg'
-            : $classes .= '';
+$classes = ($active ?? false)
+    ? 'text-center bg-gray-700 border-b-4 border-blue-300 py-6 hover:bg-gray-700 min-w-full'
+    : 'text-center bg-gray-800 py-6 hover:bg-gray-700 hover:border-blue-300 min-w-full';
+
+$classes .= ($first ?? false)
+    ? ' rounded-t-lg'
+    : '';
+
+$classes .= ($last ?? false)
+    ? ' rounded-b-lg'
+    : '';
 
 @endphp
 
