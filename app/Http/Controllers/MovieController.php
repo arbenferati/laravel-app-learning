fc45008d1409755857d7d264edc27e943e725af4
Arben Ferati <arben@ferati.org>
2021-03-26 14:53:32 +0100

    Added more info into single view (casting, similar movies)

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,20 +14,31 @@ class MovieController extends Controller
      */
     public function index()
     {
-        $popular = Http::get('http://api.themoviedb.org/3/movie/popular?api_key=' . config('services.tmdb.api') . '&language=fr-FR')->json();
-        $upcoming = Http::get('http://api.themoviedb.org/3/movie/upcoming?api_key=' . config('services.tmdb.api') . '&language=fr-FR')->json();
-        return view('pages.application.movies.movies', ['popular_movies' => $popular, 'upcoming_movies' => $upcoming]);
+        $popular = Http::get('http://api.themoviedb.org/3/movie/popular?api_key=' . config('services.tmdb.api') . '&language=en-US')->json();
+        $upcoming = Http::get('http://api.themoviedb.org/3/movie/upcoming?api_key=' . config('services.tmdb.api') . '&language=en-US')->json();
+        $toprated = Http::get('http://api.themoviedb.org/3/movie/top_rated?api_key=' . config('services.tmdb.api') . '&language=en-US')->json();
+        return view('pages.application.movies.movies', [
+            'popular_movies' => $popular,
+            'upcoming_movies' => $upcoming,
+            'toprated_movies' => $toprated,
+        ]);
     }
 
     public function single($id)
     {
-        $movie = Http::get('http://api.themoviedb.org/3/movie/' . $id . '?api_key=' . config('services.tmdb.api') . '&language=fr-FR')->json();
-        return view('pages.application.movies.single', ['movie' => $movie]);
+        $movie = Http::get('http://api.themoviedb.org/3/movie/' . $id . '?api_key=' . config('services.tmdb.api') . '&language=en-US')->json();
+        $credits = Http::get('http://api.themoviedb.org/3/movie/' . $id . '/credits?api_key=' . config('services.tmdb.api') . '&language=en-US')->json();
+        $similar = Http::get('http://api.themoviedb.org/3/movie/' . $id . '/similar?api_key=' . config('services.tmdb.api') . '&language=en-US')->json();
+        return view('pages.application.movies.single', [
+            'movie' => $movie,
+            'credits' => $credits,
+            'similar' => $similar,
+        ]);
     }
 
     public function search(Request $request)
     {
-        $movies =  Http::get('http://api.themoviedb.org/3/search/movie?api_key=' . config('services.tmdb.api') . '&language=fr-FR&include_adult=false&query=' . $request->search_movie)->json();
+        $movies =  Http::get('http://api.themoviedb.org/3/search/movie?api_key=' . config('services.tmdb.api') . '&language=en-US&include_adult=false&query=' . $request->search_movie)->json();
         return view('pages.application.movies.movies', ['search_result' => $movies]);
     }
 
--- resources/views/pages/application/movies/single.blade.php
+++ resources/views/pages/application/movies/single.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="slot">
         <div class="p-4 flex flex-row items-start">
-            <img class="h-full mb-4 rounded-l-2xl border border-r-8 border-gray-900" src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] }}" alt="" srcset="">
+            <img class="h-full mb-4 shadow-md rounded-l-2xl border border-r-8 border-gray-900" src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] }}" alt="" srcset="">
             <div class="px-8">
                 <div class="mb-4">
                     <h1 class="text-3xl uppercase tracking-widest">
@@ -21,5 +21,45 @@
             </div>
             <img class="h-full mb-4 rounded shadow-xl" src="https://image.tmdb.org/t/p/w500/{{ $movie['backdrop_path'] }}" alt="" srcset="">
         </div>
+
+        <h1 class="text-2xl text-gray-900 px-8 pt-8">Casting</h1>
+        <div class="grid grid-cols-2 lg:grid-cols-8 gap-8 p-8 items-start">
+            @foreach ($credits['cast'] as $actor)
+            <div class="mt-2 rounded-t-xl overflow-hidden shadow">
+                <a href="{{ url('/app/movies/actors' . '/' . $actor['id']) }}">
+                    <img class="h-full hover:opacity-80 transition" src="https://image.tmdb.org/t/p/w500/{{ $actor['profile_path'] }} " alt="" srcset="">
+                </a>
+                <div class="p-4">
+                    <p class="text-lg font-semibold truncate">
+                        <a href="{{ url('/app/movies/actors' . '/' . $actor['id']) }}">
+                            {{ $actor['name'] }}
+                        </a>
+                    </p>
+                    <p class="flex flex-col py-2 ">
+                        <span class="mb-2 text-md text-gray-700">Played as : {{ $actor['character'] }}</span>
+                    </p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        @if (count($similar['results']) > 0)
+        <h1 class="text-2xl text-gray-900 px-8 pt-8">Similar movies</h1>
+        <div class="grid grid-cols-2 lg:grid-cols-8 gap-8 p-8 items-start">
+            @foreach ($similar['results'] as $similar_movie)
+            <div class="mt-2 rounded-t-xl overflow-hidden shadow">
+                <a href="{{ url('/app/movies' . '/' . $similar_movie['id']) }}">
+                    <img class="h-full hover:opacity-80 transition" src="https://image.tmdb.org/t/p/w500/{{ $similar_movie['poster_path'] }} " alt="" srcset="">
+                </a>
+                <div class="p-4">
+                    <p class="text-lg font-semibold truncate"><a href="{{ url('/app/movies' . '/' . $similar_movie['id']) }}">{{ $similar_movie['title'] }}</a></p>
+                    <p class="flex flex-col py-2 ">
+                        <span class="mb-2 text-xs text-gray-500">{{ Carbon\Carbon::parse($similar_movie['release_date'])->format('M d, Y') }}</span>
+                        <span class="font-semibold">{{ $similar_movie['vote_average'] }}/10 <span class="text-xs">({{ $similar_movie['vote_count'] }} votes)</span></span>
+                    </p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        @endif
     </x-slot>
 </x-app-layout>
